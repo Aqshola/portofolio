6dfe52707a7bbb984c86300470494064d4d99be2
aqshola <aqsholafid2@gmail.com>
2022-11-18 23:38:10 +0700

    fix: change layout

--- components/SectionV2/About/Index.tsx
+++ components/SectionV2/About/Index.tsx
@@ -49,7 +49,7 @@ export default function Index() {
         </motion.p>
         <motion.p variants={opacityTransition} className="mt-5 leading-relaxed">
           You can see more {'"about me"'} in the next section or just check on {" "}
-          <a href="https://drive.google.com/file/d/1OszXSce3oFGXFKHURJu8B_oXfp0Oq7N1/view?usp=share_link" className="transition-all underline cursor-pointer border-black">
+          <a href="https://drive.google.com/file/d/1fvxdBQ66Dol9TiwW3IV03RltfoG38E7W/view?usp=share_link" className="transition-all underline cursor-pointer border-black">
             my resume
           </a>
         </motion.p>
